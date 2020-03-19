be5b4f74b7e0a55bd65382472a9f8ccb597e3176
Hull-Coleen <ch.hull@verizon.net>
2020-03-19 08:21:12 -0400

    final project stuff

--- httpRequest.js
+++ httpRequest.js
@@ -1,79 +1,136 @@
 const appKey = "4fdb2d8339feaef5789bedc8a5c9b43d";
 		var url = "https://api.openweathermap.org/data/2.5/weather?q=";
+		var url1 = "https://api.openweathermap.org/data/2.5/forecast?q="
 		var xmlReponseVar;
 		var JsonText;
-		
-		function requestData() {
-		   var searchInput = $("#city").val();
-		   if (searchInput == undefined || searchInput == ""){
-			   searchInput = "london";
-		   }
-        
-		    $.get(url + searchInput + "&appid=" + appKey, {
-			})
-				.done(function(data) {
-				console.log( data);
-				console.log( data.wind.speed);
-				console.log( data.main.temp);
-				console.log( data.weather[0].description);
-				JsonText = data;
-					
-			 });
+		const appK = "44f74c43b24d79f50ebdea4869aa071d";
+	
+    function setLocalStoreage(searchInput) {
+		console.log("local storeage function");
+		if (searchInput !== undefined || searchInput !== "") {
+			 localStorage.setItem("city", searchInput);
 		}
-	    function xmlRequest() {
-		   var searchInput = $("#city").val();
-		   if (searchInput == undefined || searchInput == ""){
-			   searchInput = "london";
-		   }
-		   var searchLink = url + searchInput + "&appid=" + appKey;
-		   var httpRequest = new XMLHttpRequest();
-			//executes on XMLHttpRequest object 
-		    httpRequest.onreadystatechange = () => {
-			//When readyState = 4 and  status property = 200, the (API data) response is ready
-		    if (httpRequest.readyState == 4 && httpRequest.status == 200)
-				    xmlReponseVar = httpRequest.responseText;
-			    }
-			//uses GET method, opens API URL, true for asynchronous 
-			httpRequest.open("GET", searchLink, true);
-			httpRequest.send();
-                	
+	}
+	function getLocalStoreage() {
+		var city = localStorage.getItem("city");
+		if (city !== undefined || city !== "") {
+		   document.getElementById("city").placeholder = city; 
 		}
-		function test() {
-				console.log("before parse" + xmlReponseVar);
-				var parsedWeather = JSON.parse(xmlReponseVar);
-				
-				var t = (parsedWeather.main.temp - 273.15) * 9/5 +32;
-				var m = (parsedWeather.main.temp_max - 273.15) * 9/5 +32;
-				var min = (parsedWeather.main.temp_min - 273.15) * 9/5 +32;
-				var feel = (parsedWeather.main.feels_like - 273.15) * 9/5 +32;
-				var t2 = t.toFixed(2);
-				var m2 = m.toFixed(2);
-				var min2 = min.toFixed(2);
-				var feel2 = feel.toFixed(2);
-				$("#status").html("You are located at " + parsedWeather.coord.lon + " lon " 
-				+ parsedWeather.coord.lat + "<br> your temp is " + t2 + " Feels like " +  feel2
-				+ " max temp " + m2 + " min Temp " + min2 + " <br> description " 
-				+ parsedWeather.weather[0].description + " wind speed " + parsedWeather.wind.speed 
-				+  " country " + parsedWeather.sys.country + " city " + parsedWeather.name);
-				 console.log(parsedWeather.wind.speed);
-				 console.log(parsedWeather.main.temp);
-				console.log(parsedWeather.weather[0].description);
+	}	
+	function requestData() {
+		var searchInput = $("#city").val();
+		if (searchInput == undefined || searchInput == ""){
+			 searchInput = "london";
 		}
-		function test2() {
-			    var t = (JsonText.main.temp - 273.15) * 9/5 +32;
-				var m = (JsonText.main.temp_max - 273.15) * 9/5 +32;
-				var min = (JsonText.main.temp_min - 273.15) * 9/5 +32;
-				var feel = (JsonText.main.feels_like - 273.15) * 9/5 +32;
-				var t2 = t.toFixed(2);
-				var m2 = m.toFixed(2);
-				var min2 = min.toFixed(2);
-				var feel2 = feel.toFixed(2);
-			$("#status").html("XML You are located at " + JsonText.coord.lon + " lon " 
-				+ JsonText.coord.lat + "<br> your temp is " + t2 + " Feels like " +  feel2
-				+ " max temp " + m2 + " min Temp " + min2 + " <br> description " 
-				+ JsonText.weather[0].description + " wind speed " + JsonText.wind.speed 
-				+  " country " + JsonText.sys.country + " city " + JsonText.name);
+        setLocalStoreage(searchInput);
+		$.getJSON(url1 + searchInput + "&cnt=5" + "&appid=" + appK, {
+		})
+			.done(function(data) {
+			console.log( data);
+			console.log( data.list[0].main.temp);
+			console.log( data.list[1].main.temp);
+			console.log( data.list[2].main.temp);
+			console.log( data.list[3].main.temp);
+			console.log( data.list[0].weather[0].description);
 
+			JsonText = data;
+			parseData();
+					
+		});
+	
+			
+	}
+	function exists(id) {
+		var element = document.getElementById(id);
+ 
+		//If it isn't "undefined" and it isn't "null", then it exists.
+		if(typeof(element) != 'undefined' && element != null){
+			return true;
+		} else{
+			return false;
 		}
+	}
+	function farenheit(temp) {
+		Ftemp = (temp - 273.15) * 9/5 + 32;
+		return Ftemp.toFixed(0);
+	}
+	    function hide() {
+		
+			$(".item3").hide();
+			$(".item4").hide();
+			$(".item5").hide();
+			$(".item6").hide();
+			$(".item7").hide();
+
+	}
+	function show() {
+			$(".item3").show();
+			$(".item4").show();
+			$(".item5").show();
+			$(".item6").show();
+			$(".item7").show();
+	}
+		
+	function parseData() {
+		var count = 0;
+		if (exists(0)) {
+			console.log("true")
+			for(i=0; i < 5; i++) {
+				
+				var li = document.getElementById(i + count );
+				count++;
+					var li1 = document.getElementById(i + count );
+					count++;
+					var li2 = document.getElementById(i + count);
+					count++
+					var iconcode = JsonText.list[i].weather[0].icon;
+					var image = document.getElementById("image" + i);
+				    image.src = "http://openweathermap.org/img/w/" + iconcode + ".png";
+				    li.innerHTML = "Day " + (i + 1);
+				    li1.innerHTML = farenheit(JsonText.list[i].main.temp);
+				    li2.innerHTML = JsonText.list[i].weather[0].description;
+			}
+		}
+		else {
+			for(i=0; i < 5; i++) {
+					var a = "day" + (i + 1);
+					var ul = document.getElementById(a);
+					var b = i + 1;
+					li = document.createElement("li");
+					li.setAttribute("id", i + count);
+					console.log(li.id);
+					count++;
+					var li1 = document.createElement("li");
+					li1.setAttribute("id", i + count);
+					console.log(li1.id);
+					count++;
+					var li2 = document.createElement("li");
+					li2.setAttribute("id", i + count);
+					console.log(li2.id);
+					count++;
+					var li3 = document.createElement("li");
+					li3.setAttribute("id", i + count);
+					console.log(li3.id);
+					var image = document.createElement("img");
+					image.setAttribute("id" , "image" + i);
+					console.log('image id ' + image.id);
+					var iconcode = JsonText.list[i].weather[0].icon;
+					image.src = "http://openweathermap.org/img/w/" + iconcode + ".png";
+					li.innerHTML = "Day " + (i + 1);
+					li1.innerHTML = farenheit(JsonText.list[i].main.temp);
+					li2.innerHTML = JsonText.list[i].weather[0].description;
+					ul.appendChild(li);
+					ul.appendChild(li1);
+					ul.appendChild(li2);
+					ul.appendChild(li3);
+					li3.appendChild(image);
+
+			}
+		}
+		$("#imageSun").hide();
+		show();
+	}
+	   
+	
 		
 	
